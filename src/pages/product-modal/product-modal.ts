4651c47621d13e0a6a4d22d98742b30b9baf984d
Sebastian Hidalgo <sgha10@hotmail.com>
2018-05-30 02:25:32 -0600

    modal product

--- src/pages/product-modal/product-modal.ts
+++ src/pages/product-modal/product-modal.ts
@@ -15,13 +15,28 @@ import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 })
 export class ProductModalPage {
 
+  //la imagen tiene que llegar por objeto
+  //"../assets/imgs/pizza.png"
   title: string;
   information: string;
+  prices: any;
+  thumbnail: string;
+  selectedSize: string;
 
-  constructor(private navParams: NavParams, private view: ViewController) {
+  product = {
+    name: "",
+    size: "P",
+    price: "",
+    observation: "",
+    quantity: "1",
+    extras: []
   }
 
-  ionViewWillLoad() {
+  constructor(private navParams: NavParams, private view: ViewController) {
+
+  }
+
+  ionViewWillEnter() {
 
   }
 
@@ -29,11 +44,45 @@ export class ProductModalPage {
     this.view.dismiss();
   }
 
-  ionViewDidLoad() {
-    console.log(this.navParams.get('data'));
-    this.title = this.navParams.get('data').name;
-    this.information = this.navParams.get('data').description;
 
+
+  ionViewDidLoad() {
+
+    this.information = this.navParams.get('data').description;
+    this.product.name = this.navParams.get('data').name;
+    this.product.price = this.navParams.get('data').price[0];
+
+    this.prices = this.navParams.get('data').price;
+    //la imagen tiene que llegar por objeto
+    this.thumbnail = "../assets/imgs/pizza.png";
+  }
+
+  changeQuantity(Type) {
+    switch (Type) {
+      case 0:
+        if (parseInt(this.product.quantity) > 1) this.product.quantity = (parseInt(this.product.quantity) - 1).toString();
+        break;
+      case 1:
+        this.product.quantity = (parseInt(this.product.quantity) + 1).toString();
+        break;
+    }
+  }
+
+  changeSize(Size) {
+    this.selectedSize = Size;
+    this.product.size = Size;
+    switch (Size) {
+      case "P":
+        this.product.price = this.prices[0];
+        break;
+      case "M":
+        this.product.price = this.prices[1];
+        break;
+      case "G":
+        this.product.price = this.prices[2];
+        break;
+    }
+    console.log(this.selectedSize);
   }
 
 }
