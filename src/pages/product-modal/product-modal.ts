50206c9a27c82b6026b6e31fa66d469ef291fce7
Sebastian Hidalgo <sgha10@hotmail.com>
2018-06-06 15:02:47 -0600

    finishing the product modal css, and adding extras

--- src/pages/product-modal/product-modal.ts
+++ src/pages/product-modal/product-modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 
 /**
@@ -15,6 +15,8 @@ import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 })
 export class ProductModalPage {
 
+  @ViewChild('observation') observation;
+
   //la imagen tiene que llegar por objeto
   //"../assets/imgs/pizza.png"
   title: string;
@@ -22,6 +24,16 @@ export class ProductModalPage {
   prices: any;
   thumbnail: string;
   selectedSize: string;
+
+  
+
+  extras = [{ name: "chili", path: "../assets/imgs/chili.png" },
+  { name: "cheese", path: "../assets/imgs/cheese.png" },
+  { name: "mushroom", path: "../assets/imgs/mushroom.png" },
+  { name: "onion", path: "../assets/imgs/onion.png" },
+  { name: "sausage", path: "../assets/imgs/sausage.png" },
+  { name: "alba", path: "../assets/imgs/alba.png" }
+  ];
 
   product = {
     name: "",
@@ -57,13 +69,37 @@ export class ProductModalPage {
     this.thumbnail = "../assets/imgs/pizza.png";
   }
 
+  extraActive(extra) {
+    let aux = this.product.extras.indexOf(extra);
+    if (aux !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  addExtra(extra) {
+    let aux = this.product.extras.indexOf(extra);
+    if (aux !== -1) {
+      this.product.extras.splice(aux, 1);
+      console.log(this.product.extras);
+    } else {
+      this.product.extras.push(extra);
+      console.log(this.product.extras);
+    }
+  }
+
+
   changeQuantity(Type) {
+    // por el momento tiene un CAP de 6 pizzas
+
     switch (Type) {
       case 0:
-        if (parseInt(this.product.quantity) > 1) this.product.quantity = (parseInt(this.product.quantity) - 1).toString();
+
+        if ((parseInt(this.product.quantity) > 1)) this.product.quantity = (parseInt(this.product.quantity) - 1).toString();
         break;
       case 1:
-        this.product.quantity = (parseInt(this.product.quantity) + 1).toString();
+        if ((parseInt(this.product.quantity) < 6)) this.product.quantity = (parseInt(this.product.quantity) + 1).toString();
         break;
     }
   }
@@ -83,6 +119,11 @@ export class ProductModalPage {
         break;
     }
     console.log(this.selectedSize);
+  } 
+
+  addProduct() {
+    this.product.observation = this.observation.value;
+    console.log(this.product);
   }
 
 }
