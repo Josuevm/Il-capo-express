a853255be608f1954ae2f0e45ca5c45707758e60
Josuevm <josuevm37@hotmail.com>
2018-07-03 14:14:38 -0600

    Extras name changed to spanish

--- src/pages/product-modal/product-modal.ts
+++ src/pages/product-modal/product-modal.ts
@@ -34,12 +34,12 @@ export class ProductModalPage {
 
 
 
-  extras = [{ name: "chili", path: "assets/imgs/chili.png" },
-  { name: "cheese", path: "assets/imgs/cheese.png" },
-  { name: "mushroom", path: "assets/imgs/mushroom.png" },
-  { name: "onion", path: "assets/imgs/onion.png" },
-  { name: "sausage", path: "assets/imgs/sausage.png" },
-  { name: "alba", path: "assets/imgs/alba.png" }
+  extras = [{ name: "Chile", path: "assets/imgs/chili.png" },
+  { name: "Queso", path: "assets/imgs/cheese.png" },
+  { name: "Hongos", path: "assets/imgs/mushroom.png" },
+  { name: "Cebolla", path: "assets/imgs/onion.png" },
+  { name: "Salchicha", path: "assets/imgs/sausage.png" },
+  { name: "Alba", path: "assets/imgs/alba.png" }
   ];
   firstHalf: string;
   secondHalf: string;
